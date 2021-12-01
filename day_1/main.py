9a1495a3a016201d4a2f6501135c2d13dd3e14ac
Fabien Le Frapper <contact@fabienlefrapper.me>
2021-12-01 12:26:07 +0100

    Deuxieme partie 💯

--- day_1/main.py
+++ day_1/main.py
@@ -2,16 +2,26 @@ import utils
 
 data = utils.read_file("./input.txt")
 
+
+def get_sum(measures, index):
+    try:
+        return measures[index] + measures[index + 1] + measures[index + 2]
+    except IndexError:
+        return 0
+
+
+def compare_windows(measures, index):
+    return get_sum(measures, index) < get_sum(measures, index + 1)
+
+
 def count(measures):
     count = 0
 
     for index, measure in enumerate(measures):
-        if index == 0:
-            pass
-
-        elif measures[index] > measures[index - 1]:
+        if compare_windows(measures, index):
             count += 1
 
     return count
+
 
 print(count(data))
