6e75abc4ee952ad19eb9d46747f8732ed414af43
Fabien Le Frapper <contact@fabienlefrapper.me>
2021-12-01 12:17:38 +0100

    Ceci explique cela.

--- day_1/utils.py
+++ day_1/utils.py
@@ -2,6 +2,6 @@ def read_file(path):
     data = []
     with open(path) as f:
         for line in f:
-            data.append(line.rstrip())
+            data.append(int(line.rstrip()))
 
     return data
